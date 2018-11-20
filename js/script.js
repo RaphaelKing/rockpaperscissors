75f4aa1897c9fa3e1b6ee89aeca01db922d2fef5
raphaelking <andreslater23@gmail.com>
2018-11-20 23:26:44 +0000

    Variables and Scenarios

--- js/script.js
+++ js/script.js
@@ -10,4 +10,55 @@
 // DOCUMENT READY FUNCTION BELOW
 $("#shoot").click(function(){
     $("#userChoice").text($("#input").val());
-});
+    var computerChoice;
+    var winner;
+    var randomNumber = Math.random();
+    var userChoice = $("#input").val();
+    if (randomNumber < .333) {
+        computerChoice = "Rock";
+    }
+    if (randomNumber > .333 && randomNumber < .666 ){
+        computerChoice = "Paper";
+    }
+    if (randomNumber > .666){
+        computerChoice = "Scissors";
+    }
+$("#computerChoice").text(computerChoice);
+    if (userChoice === "Rock" && computerChoice === "Scissors") {
+        winner = "You Won";
+    }
+    if (userChoice === "Scissors" && computerChoice === "Rock"){
+        winner = "Computer Won";
+    }
+    if (userChoice === "Rock" && computerChoice === "Paper"){
+        winner = "Computer Won";
+    }
+    if (userChoice === "Rock" && computerChoice === "Rock"){
+        winner = "NOO OONNEE";
+    }
+    if (userChoice === "Paper" && computerChoice === "Scissors"){
+        winner = "Computer Won";
+    }
+    if (userChoice === "Paper" && computerChoice === "Rock"){
+        winner = "You Won";
+    }
+    if (userChoice === "Paper" && computerChoice === "Paper"){
+        winner= "NOO OONNEE";
+    }
+    if (userChoice === "Scissors" && computerChoice === "Paper"){
+        winner= "You Won";
+    }
+    if (userChoice === "Scissors" && computerChoice === "Rock"){
+        winner= "Computer Won";
+    }
+    if (userChoice === "Scissors" && computerChoice === "Scissors"){
+        winner= "NOO OONNEE";
+    }
+$("#result").text(winner);
+})
+
+
+
+
+
+
